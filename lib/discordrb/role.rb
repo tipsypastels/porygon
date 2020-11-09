910a935c5c9ed2c496937e1f52ba3950ee91b556
tipsypastels <dakotasankey@gmail.com>
2020-11-09 00:16:09 -0800

    @everyone is not a valid from_arg role

--- lib/discordrb/role.rb
+++ lib/discordrb/role.rb
@@ -1,8 +1,10 @@
 module Discordrb
   class Role
     MENTION_FORMAT = /^<@&(\d+)>$/
+    EVERYONE = '@everyone'.freeze
 
     def self.from_argument(error, name, command)
+      error[:everyone, no_help: true] if name == EVERYONE
       command.server.find_role(name) || error[:nonexistant, arg: name]
     end
 
