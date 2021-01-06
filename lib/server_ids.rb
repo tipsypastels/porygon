abbb0edcdacc318542128101f1f7b408fe638c98
Dakota Sankey <dakotasankey@gmail.com>
2021-01-05 23:50:26 -0800

    fix server id?

--- lib/server_ids.rb
+++ lib/server_ids.rb
@@ -2,6 +2,6 @@ module ServerIds
   # rubocop:disable Style/NumericLiterals
   TEST = 746219046374080532
   DUCK = 322199235825238017
-  POKE = 193103073210662914
+  POKE = 157983957902819328
   # rubocop:enable Style/NumericLiterals
 end
