ca02a609d761bfa48e5e3e72b7969806c20db73c
Dakota Sankey <dakotasankey@gmail.com>
2021-02-26 13:48:59 -0800

    fix

--- lib/porygon/activity_message.rb
+++ lib/porygon/activity_message.rb
@@ -1,7 +1,7 @@
 module Porygon
   class ActivityMessage < Sequel::Model
     def self.sample
-      order(Sequel.lit('RANDOM()')).limit(1).first
+      order(Sequel.lit('RANDOM()')).limit(1).first&.message
     end
 
     def self.add(message)
